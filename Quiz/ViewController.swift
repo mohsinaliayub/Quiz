ab4a70d21bf48346aa861f8fa54ee37f9e4c5aa3
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-08-08 13:34:59 +0200

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Mohsin Ali Ayub on 08.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
